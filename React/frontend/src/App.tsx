4134eab183201543ef3e4793940feb0b52b17611
Shaun Pearson <shaun@bigwhitesolutions.com>
2022-11-05 12:11:26 +0000

    Start building Query Builder UI

--- React/frontend/src/App.tsx
+++ React/frontend/src/App.tsx
@@ -1,34 +1,65 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from "react";
 import { QueryResultJson, Query, QuerySelect, jsonifyQuery } from "flowerbi";
-import { Product, ProductSubcategory } from '../AdventureWorks2019Schema';
-import './App.css'
-import { localFetch, query } from './api-helpers';
+import { Product, ProductSubcategory } from "../AdventureWorks2019Schema";
+import "./App.css";
+import { localFetch } from "./api-helpers";
 
-const nameOf = (f: any) => (f).toString().replace(/[ |\(\)=>]/g,'');
+const TableValues = [
+  ...Object.values(Product),
+  ...Object.values(ProductSubcategory),
+];
 
-const Tables: string[] = [ nameOf(() => Product), nameOf(() => ProductSubcategory) ]
-
+const TableDefinitions = TableValues.map((x) => {
+  var nameSplit = x.name.split(".");
+  return { name: x.name, table: nameSplit[0], field: nameSplit[1] };
+});
 
 function App() {
-  var [ data, setData ] = useState<QueryResultJson | undefined>(undefined)
+  const [data, setData] = useState<QueryResultJson | undefined>(undefined);
+  const [query, setQuery] = useState<Query<QuerySelect>>({
+    select: {
+      name: ProductSubcategory.Name,
+      productCount: Product.ProductId.count(),
+    },
+    totals: true,
+  });
+
+  const getData = () =>
+    localFetch(jsonifyQuery(query)).then((x) => {
+      setData(x);
+    });
 
   useEffect(() => {
-    localFetch(jsonifyQuery(query))
-    .then(x => {
-      setData(x)
-    }
-    );
-  }, [])
+    getData();
+  }, []);
 
-  if (!data || !data.records) return <>Loading</>
+  if (!data || !data.records) return <>Loading</>;
 
   return (
-    <div>
-    {Tables.map(x=> (<div>{x}</div>))}
-    {data.records.map((x, i)=> (
-      <div key={i}><>Item:{x.selected[0]}({x.aggregated[0]})</></div>
-    ))}
-    </div>)
+    <div className="min-h-screen flex">
+      <nav className="w-56 flex-none px-2">
+        Tables
+        {TableDefinitions.map((x) => (
+          <div key={x.name}>{x.name}</div>
+        ))}
+      </nav>
+
+      <main className="flex-1 min-w-0 overflow-auto px-2">
+        {data.records.map((x, i) => (
+          <div key={i}>
+            <>
+              Item:{x.selected[0]}({x.aggregated[0]})
+            </>
+          </div>
+        ))}
+      </main>
+      <textarea
+        readOnly={true}
+        rows={10}
+        defaultValue={JSON.stringify(jsonifyQuery(query))}
+      ></textarea>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- React/frontend/src/api-helpers.ts
+++ React/frontend/src/api-helpers.ts
@@ -1,23 +1,14 @@
-import { Query, QueryJson, QueryResultJson, QuerySelect } from "flowerbi";
-import { ProductSubcategory, Product } from "../AdventureWorks2019Schema";
+import { QueryJson, QueryResultJson, QuerySelect } from "flowerbi";
 
-export async function localFetch(queryJson: QueryJson): Promise<QueryResultJson> {
-
-    const response = await fetch("/api/QueryDatabase", {
-        method: "POST",
-        cache: "no-cache",
-        mode: 'cors',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(queryJson),
-    });
-    return !response.ok ? [] : JSON.parse(await response.text());
-  }
-  
-  export const query: Query<QuerySelect> = {
-    select: {
-      name: ProductSubcategory.Name,
-      productCount:Product.ProductId.count()
-    },  
-    totals: true
-  
-  }
+export async function localFetch(
+  queryJson: QueryJson
+): Promise<QueryResultJson> {
+  const response = await fetch("/api/QueryDatabase", {
+    method: "POST",
+    cache: "no-cache",
+    mode: "cors",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(queryJson),
+  });
+  return !response.ok ? [] : JSON.parse(await response.text());
+}
